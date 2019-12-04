02fd4c27e8e3be66e1808b8e165dcf544f94bc6c
vinicius <viniciussf@hotmail.com>
2019-12-04 06:26:11 -0300

    Relatorio de digitos de unmatching

--- src/main/java/br/com/inteligenciaartificial/algoritmos/math/Matrix.java
+++ src/main/java/br/com/inteligenciaartificial/algoritmos/math/Matrix.java
@@ -5,250 +5,244 @@ import java.util.function.DoublePredicate;
 import java.util.function.DoubleUnaryOperator;
 
 public class Matrix {
+	private int colNum = -1;
+	private double[][] elements;
 
-    public static void main(final String[] args) {
-        final Matrix m1 = new Matrix(new double[] {2, 3}, new double[] {4, 5});
-        final Matrix m2 = new Matrix(new double[] {1, 2}, new double[] {3, 2});
+	private int rowNum = -1;
 
-        m1.multiply(m2).print();
-    }
+	public Matrix() {
+	}
 
-    private int colNum = -1;
-    private double[][] elements;
+	public Matrix(final double[]... rows) {
+		rowNum = rows.length;
+		colNum = rows[0].length;
+		elements = new double[rowNum][];
 
-    private int rowNum = -1;
+		for (int i = 0; i < rowNum; i++) {
+			if (rows[i].length != colNum) {
+				throw new IllegalArgumentException("All rows must have the same length");
+			}
+			elements[i] = rows[i];
+		}
+	}
 
-    public Matrix() {}
+	public Matrix(final int rowNum, final int colNum) {
+		elements = new double[rowNum][colNum];
+		this.rowNum = rowNum;
+		this.colNum = colNum;
+	}
 
-    public Matrix(final double[]... rows) {
-        rowNum = rows.length;
-        colNum = rows[0].length;
-        elements = new double[rowNum][];
+	Matrix addRow(final double[] row) {
+		if (rowNum < 0) {
+			rowNum = 1;
+			colNum = row.length;
+			elements = new double[1][];
+			elements[0] = row;
+			return this;
+		}
 
-        for (int i = 0; i < rowNum; i++) {
-            if (rows[i].length != colNum) {
-                throw new IllegalArgumentException("All rows must have the same length");
-            }
-            elements[i] = rows[i];
-        }
-    }
+		if (row.length != colNum) {
+			throw new IllegalArgumentException("All rows must have the same length");
+		} else {
+			rowNum++;
+			final double[][] copy = new double[rowNum][];
+			for (int r = 0; r < elements.length; r++) {
+				copy[r] = elements[r];
+			}
+			copy[rowNum - 1] = row;
+			elements = copy;
+		}
+		return this;
+	}
 
-    public Matrix(final int rowNum, final int colNum) {
-        elements = new double[rowNum][colNum];
-        this.rowNum = rowNum;
-        this.colNum = colNum;
-    }
+	public Matrix apply(final DoubleUnaryOperator operation) {
+		final double[][] result = new double[rowNum][colNum];
 
-    Matrix addRow(final double[] row) {
-        if (rowNum < 0) {
-            rowNum = 1;
-            colNum = row.length;
-            elements = new double[1][];
-            elements[0] = row;
-            return this;
-        }
+		for (int r = 0; r < elements.length; r++) {
+			for (int c = 0; c < elements[r].length; c++) {
+				result[r][c] = operation.applyAsDouble(elements[r][c]);
+			}
+		}
+		return new Matrix(result);
+	}
 
-        if (row.length != colNum) {
-            throw new IllegalArgumentException("All rows must have the same length");
-        } else {
-            rowNum++;
-            final double[][] copy = new double[rowNum][];
-            for (int r = 0; r < elements.length; r++) {
-                copy[r] = elements[r];
-            }
-            copy[rowNum - 1] = row;
-            elements = copy;
-        }
-        return this;
-    }
+	public Matrix copy() {
+		return new Matrix(Arrays.copyOf(elements, elements.length));
+	}
 
-    public Matrix copy() {
-        return new Matrix(Arrays.copyOf(elements, elements.length));
-    }
+	public Matrix dot(final Matrix other) {
+		if (other == null) {
+			return null;
+		}
+		if (rowNum != other.rowNum || colNum != other.colNum) {
+			throw new IllegalArgumentException(String.format(
+					"You tried operate a matrix %s and another matrix %s, but both must have the same rom and column numbers",
+					order(), other.order()));
+		}
+		final Matrix m = new Matrix(rowNum, colNum);
+		for (int r = 0; r < elements.length; r++) {
+			for (int c = 0; c < elements[r].length; c++) {
+				m.set(r, c, elements[r][c] * other.elements[r][c]);
+			}
+		}
+		return m;
+	}
 
-    public Matrix dot(final Matrix other) {
-        if (other == null) {
-            return null;
-        }
-        if (rowNum != other.rowNum || colNum != other.colNum) {
-            throw new IllegalArgumentException(String.format(
-                            "You tried operate a matrix %s and another matrix %s, but both must have the same rom and column numbers",
-                            order(), other.order()));
-        }
-        final Matrix m = new Matrix(rowNum, colNum);
-        for (int r = 0; r < elements.length; r++) {
-            for (int c = 0; c < elements[r].length; c++) {
-                m.set(r, c, elements[r][c] * other.elements[r][c]);
-            }
-        }
-        return m;
-    }
+	@Override
+	public boolean equals(final Object other) {
+		if (other == null || !(other instanceof Matrix)) {
+			return false;
+		}
 
-    public double get(final int rowIndex, final int colIndex) {
-        return elements[rowIndex][colIndex];
-    }
+		final Matrix m = (Matrix) other;
+		if (elements == null || m.elements == null || rowNum != m.rowNum || colNum != m.colNum) {
+			return false;
+		}
 
-    public int getColNum() {
-        return colNum;
-    }
+		for (int i = 0; i < rowNum; i++) {
+			for (int j = 0; j < colNum; j++) {
+				if (elements[i][j] != m.elements[i][j]) {
+					return false;
+				}
+			}
+		}
 
-    public int getRowNum() {
-        return rowNum;
-    }
+		return true;
+	}
 
-    public Matrix initRandom() {
-        for (int r = 0; r < rowNum; r++) {
-            for (int c = 0; c < colNum; c++) {
-                elements[r][c] = Math.random();
-            }
-        }
-        return this;
-    }
+	public double get(final int rowIndex, final int colIndex) {
+		return elements[rowIndex][colIndex];
+	}
 
-    public boolean test(final DoublePredicate validation) {
-        for (int r = 0; r < elements.length; r++) {
-            for (int c = 0; c < elements[r].length; c++) {
-                if (!validation.test(elements[r][c])) {
-                    return false;
-                }
-            }
-        }
-        return true;
-    }
+	public int getColNum() {
+		return colNum;
+	}
 
-    public Matrix module() {
-        return apply(Math::abs);
-    }
+	public int getRowNum() {
+		return rowNum;
+	}
 
-    public Matrix multiply(final Matrix other) {
-        if (colNum != other.rowNum) {
-            throw new UnsupportedOperationException("Operation is not defined. Matriz: " + order() + " and Matrix: " + other.order());
-        }
+	public Matrix initRandom() {
+		for (int r = 0; r < rowNum; r++) {
+			for (int c = 0; c < colNum; c++) {
+				elements[r][c] = Math.random();
+			}
+		}
+		return this;
+	}
 
-        final Matrix m = new Matrix(rowNum, other.colNum);
-        double sum = 0;
-        for (int r = 0; r < elements.length; r++) {
+	public Matrix module() {
+		return apply(Math::abs);
+	}
 
-            for (int oCol = 0; oCol < other.colNum; oCol++) {
-                for (int oRow = 0; oRow < other.rowNum; oRow++) {
-                    sum += elements[r][oRow] * other.elements[oRow][oCol];
-                }
-                m.set(r, oCol, sum);
-                sum = 0;
-            }
+	public Matrix multiply(final Matrix other) {
+		if (colNum != other.rowNum) {
+			throw new UnsupportedOperationException(
+					"Operation is not defined. Matriz: " + order() + " and Matrix: " + other.order());
+		}
 
-        }
-        return m;
-    }
+		final Matrix m = new Matrix(rowNum, other.colNum);
+		double sum = 0;
+		for (int r = 0; r < elements.length; r++) {
 
-    public Matrix apply(final DoubleUnaryOperator operation) {
-        final double[][] result = new double[rowNum][colNum];
+			for (int oCol = 0; oCol < other.colNum; oCol++) {
+				for (int oRow = 0; oRow < other.rowNum; oRow++) {
+					sum += elements[r][oRow] * other.elements[oRow][oCol];
+				}
+				m.set(r, oCol, sum);
+				sum = 0;
+			}
 
-        for (int r = 0; r < elements.length; r++) {
-            for (int c = 0; c < elements[r].length; c++) {
-                result[r][c] = operation.applyAsDouble(elements[r][c]);
-            }
-        }
-        return new Matrix(result);
-    }
+		}
+		return m;
+	}
 
-    public String order() {
-        return rowNum + "X" + colNum;
-    }
+	public String order() {
+		return rowNum + "X" + colNum;
+	}
 
-    public void print() {
-        System.out.println(toString());
-    }
+	public void print() {
+		System.out.println(toString());
+	}
 
-    public Matrix set(final int rowIndex, final int colIndex, final double value) {
-        elements[rowIndex][colIndex] = value;
-        return this;
-    }
+	public Matrix set(final int rowIndex, final int colIndex, final double value) {
+		elements[rowIndex][colIndex] = value;
+		return this;
+	}
 
-    public Matrix sub(final Matrix other) {
-        return sum(other, false);
-    }
+	public Matrix sub(final Matrix other) {
+		return sum(other, false);
+	}
 
-    public Matrix sum(final Matrix other) {
-        return sum(other, true);
-    }
+	public Matrix sum(final Matrix other) {
+		return sum(other, true);
+	}
 
-    private Matrix sum(final Matrix other, final boolean isPlus) {
-        if (other == null) {
-            throw new UnsupportedOperationException("Sum operation is no defined to a null matrix");
+	private Matrix sum(final Matrix other, final boolean isPlus) {
+		if (other == null) {
+			throw new UnsupportedOperationException("Sum operation is no defined to a null matrix");
 
-        }
+		}
 
-        if (rowNum != other.rowNum || colNum != other.colNum) {
-            throw new UnsupportedOperationException(
-                            String.format("Sum operation is no defined between matrix %s and another matrix %s", order(), other.order()));
-        }
+		if (rowNum != other.rowNum || colNum != other.colNum) {
+			throw new UnsupportedOperationException(String.format(
+					"Sum operation is no defined between matrix %s and another matrix %s", order(), other.order()));
+		}
 
-        final Matrix m = new Matrix(rowNum, colNum);
-        for (int r = 0; r < rowNum; r++) {
-            for (int c = 0; c < colNum; c++) {
-                m.set(r, c, isPlus ? elements[r][c] + other.elements[r][c] : elements[r][c] - other.elements[r][c]);
-            }
-        }
-        return m;
-    }
+		final Matrix m = new Matrix(rowNum, colNum);
+		for (int r = 0; r < rowNum; r++) {
+			for (int c = 0; c < colNum; c++) {
+				m.set(r, c, isPlus ? elements[r][c] + other.elements[r][c] : elements[r][c] - other.elements[r][c]);
+			}
+		}
+		return m;
+	}
 
-    @Override
-    public String toString() {
-        final StringBuilder m = new StringBuilder("Matrix ").append(order()).append(": ");
-        final StringBuilder space = new StringBuilder();
+	public boolean test(final DoublePredicate validation) {
+		for (int r = 0; r < elements.length; r++) {
+			for (int c = 0; c < elements[r].length; c++) {
+				if (!validation.test(elements[r][c])) {
+					return false;
+				}
+			}
+		}
+		return true;
+	}
 
-        for (int i = 0; i < m.length(); i++) {
-            space.append(" ");
-        }
+	@Override
+	public String toString() {
+		final StringBuilder m = new StringBuilder("Matrix ").append(order()).append(": ");
+		final StringBuilder space = new StringBuilder();
 
-        for (int r = 0; r < elements.length; r++) {
+		for (int i = 0; i < m.length(); i++) {
+			space.append(" ");
+		}
 
-            for (int c = 0; c < elements[r].length; c++) {
-                m.append(elements[r][c]);
-                if (c < elements[r].length - 1) {
-                    m.append(" ");
-                }
-            }
-            if (r < elements.length - 1) {
-                m.append("\n");
-                m.append(space);
-            }
+		for (int r = 0; r < elements.length; r++) {
 
-        }
-        return m.toString();
-    }
+			for (int c = 0; c < elements[r].length; c++) {
+				m.append(elements[r][c]);
+				if (c < elements[r].length - 1) {
+					m.append(" ");
+				}
+			}
+			if (r < elements.length - 1) {
+				m.append("\n");
+				m.append(space);
+			}
 
-    public Matrix transpose() {
-        final double[][] copy = new double[colNum][rowNum];
-        for (int r = 0; r < rowNum; r++) {
-            for (int c = 0; c < colNum; c++) {
-                copy[c][r] = elements[r][c];
-            }
-        }
+		}
+		return m.toString();
+	}
 
-        return new Matrix(copy);
-    }
+	public Matrix transpose() {
+		final double[][] copy = new double[colNum][rowNum];
+		for (int r = 0; r < rowNum; r++) {
+			for (int c = 0; c < colNum; c++) {
+				copy[c][r] = elements[r][c];
+			}
+		}
 
-    @Override
-    public boolean equals(final Object other) {
-        if (other == null || !(other instanceof Matrix)) {
-            return false;
-        }
-
-        final Matrix m = (Matrix) other;
-        if (elements == null || m.elements == null || rowNum != m.rowNum || colNum != m.colNum) {
-            return false;
-        }
-
-        for (int i = 0; i < rowNum; i++) {
-            for (int j = 0; j < colNum; j++) {
-                if (elements[i][j] != m.elements[i][j]) {
-                    return false;
-                }
-            }
-        }
-
-        return true;
-    }
+		return new Matrix(copy);
+	}
 }
--- src/main/java/br/com/inteligenciaartificial/algoritmos/neural/Main.java
+++ src/main/java/br/com/inteligenciaartificial/algoritmos/neural/Main.java
@@ -5,43 +5,58 @@ import java.util.Date;
 import java.util.List;
 
 public class Main {
-    private static final String BAR = "-------------------------";
+	private static final String BAR = "-------------------------";
 
-    public static void main(final String[] args) throws IOException {
-        final List<TrainingDigit> trainingData =
-                        new MnistDataReader().readData("data/train-images.idx3-ubyte", "data/train-labels.idx1-ubyte");
+	public static void main(final String[] args) throws IOException {
 
-        final DigitClassifier classifier = new DigitClassifier();
-        System.out.println(BAR);
-        System.out.println("LEARNING IS JUST BEGINNIG");
-        System.out.println(BAR);
+		final List<TrainingDigit> trainingData = new MnistDataReader().readData("data/train-images.idx3-ubyte",
+				"data/train-labels.idx1-ubyte");
 
-        final Date init = new Date();
+		final DigitClassifier classifier = new DigitClassifier();
+		System.out.println(BAR);
+		System.out.println("LEARNING IS JUST BEGINNIG");
+		System.out.println(BAR);
 
-        classifier.training(trainingData);
+		final Date init = new Date();
 
-        final Date end = new Date();
+		classifier.training(trainingData);
 
-        System.out.println(String.format("LEARNING IS COMPLETED in %d secs.", (end.getTime() - init.getTime()) / 1000));
-        System.out.println(BAR);
+		final Date end = new Date();
 
-        TrainingDigit data = null;
+		System.out.println(String.format("LEARNING IS COMPLETED in %d secs.", (end.getTime() - init.getTime()) / 1000));
+		System.out.println(BAR);
 
-        int result = -1;
-        int matches = 0;
-        final int total = 10000;
-        for (int i = 0; i < total; i++) {
-            data = trainingData.get(i);
-            System.out.println(String.format("The digit is %d, and was classified as %d", data.getValue(),
-                            result = classifier.classify(data)));
-            if (result == data.getValue()) {
-                matches++;
-            }
+		final List<TrainingDigit> testingData = new MnistDataReader().readData("data/t10k-images.idx3-ubyte",
+				"data/t10k-labels.idx1-ubyte");
 
-        }
-        System.out.println(BAR);
-        System.out.println(String.format("Total matching %d from %d", matches, total));
-        System.out.println(BAR);
+		int result = -1;
+		int matches = 0;
+		final int[] unmatchs = new int[10];
+		final int[] total = new int[10];
 
-    }
+		for (final TrainingDigit digit : testingData) {
+			result = classifier.classify(digit);
+			total[digit.getValue()]++;
+
+			if (result == digit.getValue()) {
+				matches++;
+			} else {
+				unmatchs[digit.getValue()]++;
+			}
+		}
+
+		System.out.println(BAR);
+		System.out.println(String.format("Total matching %d from %d.", matches, testingData.size()));
+		System.out.println("Unmatching report: ");
+		for (int i = 0; i < unmatchs.length; i++) {
+			System.out.println(String.format("Digit %d unmatching %d from %d. Result %.2f %s", i, unmatchs[i], total[i],
+					Main.percent(unmatchs[i], total[i]), "%"));
+		}
+		System.out.println(BAR);
+
+	}
+
+	private static double percent(final double a, final double b) {
+		return a * 100d / b;
+	}
 }
